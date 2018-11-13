f1133222d6686d4050b4514f3bad477315427efd
LT_lrsoft <lrsoft@hotmail.com>
2018-11-13 19:40:19 +0800

    v1.1(alpha
    N/A

--- BankSystem_test/basic_func.h
+++ BankSystem_test/basic_func.h
@@ -8,6 +8,7 @@
 typedef struct {
 	int cardnum[3] = {0,0,0};
 	long double money[3] = {0.0,0.0,0.0};
+	char id_card[19];
 	char name[7];
 	char pwd[7];
 }PData;
@@ -57,6 +58,18 @@ SInfo *SearchByNum(SInfo *head, int cnum) {
 	printf("无法找到该用户！请检查卡号是否输入错误！\n");
 	return NULL;
 }
+SInfo *SearchById(SInfo *head,char id_card[]) {
+	SInfo *htemp;
+	htemp = head;
+	while (htemp) {
+		if (strcmp(htemp->data.id_card,id_card)==0) {
+			return htemp;
+		}
+		htemp = htemp->nextInfo;
+	}
+	printf("无法根据Id卡信息得到数据！请检查是否Id号输入有误！\n");
+	return NULL;
+}
 SearchInfo *SearchByName(SInfo *head,char name[]) {//记得free()
 	int len = 0;
 	SInfo *htemp;
@@ -100,6 +113,7 @@ SInfo *SInfoInit(SInfo *head) {
 	temp.money[0] = -88888888.0;
 	strcpy(temp.name, "FILLED");
 	strcpy(temp.pwd, "ABCDEF");
+	strcpy(temp.id_card, "440105100000000000");
 	head = AddToList(head, temp);
 	return head;
 }
@@ -117,9 +131,42 @@ void ShowAllInfo(SInfo *head) {
 	SInfo *htemp;
 	htemp = head;
 	while (htemp) {
-		printf("信息:[(%d,%d,%d),%s,%s]",htemp->data.cardnum[0], htemp->data.cardnum[1], htemp->data.cardnum[2], htemp->data.name, htemp->data.pwd);
+		printf("Info:[卡号:(%d,%d,%d),姓名:%s,密码:%s,ID:%s]",htemp->data.cardnum[0], htemp->data.cardnum[1], htemp->data.cardnum[2], htemp->data.name, htemp->data.pwd,htemp->data.id_card);
 		printf("￥:[%lf,%lf,%lf]\n",htemp->data.money[0], htemp->data.money[1], htemp->data.money[2]);
 		htemp = htemp->nextInfo;
+	}
+}
+int Deposit(SInfo *head, int card, long double money) {
+	SInfo *temp; int  i;
+	temp = SearchByNum(head,card);
+	if (temp == NULL) return -1;
+	for (i = 0; i < 3;i++) {
+		if (temp->data.cardnum[i]==card) {
+			temp->data.money[i] += money;
+			return 1;
+		}
+	}
+	return 0;
+}
+int DrawMoney(SInfo *head,int card,char pwd[],long double money) {
+	SInfo *temp; int  i;
+	temp = SearchByNum(head, card);
+	if (temp == NULL) return -1;
+	if (strcmp(temp->data.pwd,pwd)==0) {
+		for (i = 0; i < 3;i++) {
+			if (temp->data.cardnum[i]==card) {
+				if (temp->data.money[i]-money>=0) {
+					//getMoney(money);
+					temp->data.money[i] -= money;
+					return 1;
+				}
+				else {
+					return 0;
+				}
+			}
+		}
+	}else {
+		return -1;
 	}
 }
 int SaveInfo(SInfo *head) {
@@ -133,6 +180,7 @@ int SaveInfo(SInfo *head) {
 			fwrite(&htemp->data.cardnum[i],sizeof(int),1,fp);
 			fwrite(&htemp->data.money[i], sizeof(long double), 1, fp);
 		}
+		fwrite(htemp->data.id_card, sizeof(htemp->data.id_card), 1, fp);
 		fwrite(htemp->data.name,sizeof(htemp->data.name),1,fp);
 		fwrite(htemp->data.pwd, sizeof(htemp->data.pwd), 1, fp);
 		htemp = htemp->nextInfo;
@@ -152,6 +200,7 @@ SInfo *ReadInfo() {
 			fread(&tdata.cardnum[i], sizeof(int), 1, fp);
 			fread(&tdata.money[i], sizeof(long double), 1, fp);
 		}
+		fread(tdata.id_card, sizeof(tdata.id_card), 1, fp);
 		fread(tdata.name, sizeof(tdata.name), 1, fp);
 		fread(tdata.pwd, sizeof(tdata.pwd), 1, fp);
 		htemp=AddToList(htemp,tdata);
--- BankSystem_test/func.h
+++ BankSystem_test/func.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "basic_func.h"
+#include <time.h>
 int MoneyTransfer(SInfo *head,char pwd[],int cardS,int cardR,long double money) {
 	int i,tempS,tempR;
 	SInfo *accountS, *accountR;
@@ -21,7 +22,7 @@ int MoneyTransfer(SInfo *head,char pwd[],int cardS,int cardR,long double money) 
 		if (accountS->data.money[tempS]-money>=0) {
 			accountS->data.money[tempS] -= money;
 			accountR->data.money[tempR] += money;
-			printf("从%d转账%lf元到%d成功！\n",cardS,money,cardR);
+			printf("从卡号%d转账%lf元到卡号%d成功！\n",cardS,money,cardR);
 			return 1;
 		}else {
 			printf("%d卡内余额不足,转账失败！\n",cardS);
@@ -93,5 +94,73 @@ int AccountCancellation(SInfo *head,char name[],char pwd[],int card) {
 	else {
 		printf("个人信息有误！\n");
 		return 0;
+	}
+}
+int AccountDeposit(SInfo *head,int card,long double money) {
+	int temp = Deposit(head,card,money);
+	switch (temp)
+	{
+		case -1:
+			printf("存款失败！账号不存在！\n"); break;
+		case 1:
+			printf("成功向卡号%d存入%lf元。\n", card, money); break;
+		default:
+			printf("未知原因导致存款失败！\n"); break;
+	}
+	return temp;
+}
+int AccountDrawMoney(SInfo *head,int card,char pwd[],long double money) {
+	int temp = DrawMoney(head,card,pwd,money);
+	switch (temp)
+	{
+		case 0:
+			printf("余额不足，取款失败！\n"); break;
+		case 1:
+			printf("取款成功，请取走你的钱款\n"); break;
+		default:
+			printf("卡号或者密码错误！\n"); break;
+	}
+	return temp;
+}
+SInfo *CreateAccount(SInfo *head, char name[], char pwd[], char id_card[]) {
+	SInfo *temp = SearchById(head, id_card), *htemp;
+	PData info;
+	if (temp != NULL) {
+		printf("该身份id卡已经注册了账户，注册失败！\n");
+		return NULL;
+	}
+	strcpy(info.id_card, id_card);
+	strcpy(info.name, name);
+	strcpy(info.pwd, pwd);
+	htemp = AddToList(head, info);
+	if (htemp != NULL) {
+		head = htemp;
+		return head;
+	}
+	else {
+		printf("未知原因导致注册失败！\n");
+		return head;
+	}
+}
+int CreateCard(SInfo *head,char pwd[],char id_card[]) {
+	int card_num,i,remain=0;
+getRandCard:
+	srand(time(NULL));
+	card_num= rand() * 10;
+	SInfo *temp = SearchByNum(head,card_num),*htemp;
+	htemp = SearchById(head,id_card);
+	if (temp != NULL) {
+		goto getRandCard;
+	}
+	for (i = 0; i < 3;i++) {
+		if (htemp->data.cardnum[i] == 0) remain++;
+	}
+	if (remain>0) {
+		htemp->data.cardnum[3 - remain] = card_num;
+		printf("开卡成功！卡号为&d",card_num);
+		return 1;
+	}else {
+		printf("已达最大三张卡的份额！开卡失败！\n");
+		return -1;
 	}
 }
